959ef6141cadba95dbf0970a90952dd4532eb7df
lewigao <lewigao@gmail.com>
2025-11-24 10:36:10 -0500

    added: square movement + logic for squares hitting the border (bouncing + color change)

--- laughingman.js
+++ laughingman.js
@@ -1,23 +1,23 @@
-var SQUARE_COUNT = 3;
+var SQUARE_COUNT = 0;
 var SPEED = 5;
-var TIMER_SPEED = 100;
+var TIMER_SPEED = 1;
 
 document.addEventListener('DOMContentLoaded', () => {
     let squareElement = document.getElementById('square');
     // or let squareCount = document.querySelector('#square');
 
     squareElement.addEventListener('click', () => {
-        alert("touch me'RE COMING");
+        alert("OMG you CAUGHT me");
     });
 
     let box = document.querySelector('#box');
 
-    for (let i=0; i<SQUARE_COUNT; i++){
+    for (let i = 0; i < SQUARE_COUNT; i++) {
         //square creation
         let square = document.createElement('img');
-        square.src='laughing_man.jpg';
-        square.alt='Catch the lauging man!';
-        square.className='square';
+        square.src = 'laughing_man.jpg';
+        square.alt = 'Catch the laughing man!';
+        square.className = 'square';
         //adds it to the screen
         box.appendChild(square);
     }
@@ -25,8 +25,33 @@ document.addEventListener('DOMContentLoaded', () => {
     let allSquares = box.children;
     // or let allSquares = box.querySelector('box');
     let squareArray = Array.from(allSquares);
-    squareArray.forEach(function(element, index){
-        var dx = SPEED * (Math.random()*2 - 1);
-        var dy = SPEED * (Math.random()*2 - 1);
+    squareArray.forEach(function (element, index) {
+        var dx = SPEED * (Math.random() * 2 - 1);
+        var dy = SPEED * (Math.random() * 2 - 1);
+
+        setInterval(() => {
+            let currentLeft = parseInt(element.style.left) || 225;
+            let currentTop = parseInt(element.style.top) || 175;
+            
+            if (currentTop >= 350 || currentTop <= 0){
+                dy = -dy;
+                element.style.borderColor = getRandomColor();
+            }
+            if (currentLeft >= 450 || currentLeft <= 0){
+                dx = -dx;
+                element.style.borderColor = getRandomColor();
+
+            }
+            element.style.left = currentLeft+dx+"px";
+            element.style.top = currentTop+dy+"px";
+        }, TIMER_SPEED);
     });
 });
+
+function getRandomColor(){
+    let r = parseInt(Math.random()*256);
+    let g = parseInt(Math.random()*256);
+    let b = parseInt(Math.random()*256);
+    let color = `rgb(${r}, ${g}, ${b})`;
+    return color;
+}
