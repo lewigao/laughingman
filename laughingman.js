ce8cee17030b06a1212328ef252aee31db9402f3
lewigao <lewigao@gmail.com>
2025-12-02 17:11:29 -0500

    added: mouseover/mouseout logic for laughing_man squares, mouseover logic for main colored square

--- laughingman.js
+++ laughingman.js
@@ -1,6 +1,7 @@
+// 1. square count variable
 var SQUARE_COUNT = 3;
 var SPEED = 5;
-var TIMER_SPEED = 1;
+var TIMER_SPEED = 10;
 
 document.addEventListener('DOMContentLoaded', () => {
     let squareElement = document.getElementById('square');
@@ -9,18 +10,36 @@ document.addEventListener('DOMContentLoaded', () => {
     squareElement.addEventListener('click', () => {
         alert("OMG you CAUGHT me");
     });
+    
+    // 6. mouseover #square color change 
+    let coloredSquare = document.querySelector('#square');
+    coloredSquare.addEventListener('mouseover', e => {
+        square.style.backgroundColor = getRandomColor();
+    });
 
     let box = document.querySelector('#box');
 
+    // 2. square creation loop
     for (let i = 0; i < SQUARE_COUNT; i++) {
         //square creation
         let square = document.createElement('img');
+        // 3. laughing man image
         square.src = 'laughing_man.jpg';
         square.alt = 'Catch the laughing man!';
         square.className = 'square';
+        square.addEventListener('mouseover', e => {
+            square.src = 'Orochimaru.jpg';
+        });
+        // 5. mouseover orochimaru/mouseout reversion
+        square.addEventListener('mouseout', e => {
+            square.src = 'laughing_man.jpg';
+        });
         //adds it to the screen
         box.appendChild(square);
     }
+
+
+
 
     let allSquares = box.children;
     // or let allSquares = box.querySelector('box');
@@ -33,26 +52,30 @@ document.addEventListener('DOMContentLoaded', () => {
             // changed parseInt to parseFloat: fixes rubberbanding issue
             let currentLeft = parseFloat(element.style.left) || 225;
             let currentTop = parseFloat(element.style.top) || 175;
-            
-            if (currentTop >= 350 || currentTop <= 0){
+
+            // 4. border color change on boundary hit
+            if (currentTop >= 350 || currentTop <= 0) {
                 dy = -dy;
                 element.style.borderColor = getRandomColor();
             }
-            if (currentLeft >= 450 || currentLeft <= 0){
+            if (currentLeft >= 450 || currentLeft <= 0) {
                 dx = -dx;
                 element.style.borderColor = getRandomColor();
 
             }
-            element.style.left = currentLeft+dx+"px";
-            element.style.top = currentTop+dy+"px";
+            element.style.left = currentLeft + dx + "px";
+            element.style.top = currentTop + dy + "px";
         }, TIMER_SPEED);
     });
+
+
+
 });
 
-function getRandomColor(){
-    let r = parseInt(Math.random()*256);
-    let g = parseInt(Math.random()*256);
-    let b = parseInt(Math.random()*256);
+function getRandomColor() {
+    let r = parseInt(Math.random() * 256);
+    let g = parseInt(Math.random() * 256);
+    let b = parseInt(Math.random() * 256);
     let color = `rgb(${r}, ${g}, ${b})`;
     return color;
 }
